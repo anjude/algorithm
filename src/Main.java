1b6f0637486bc0a004bc4d660e4575305fabd4da
anjude <aboy007262@163.com>
2020-07-27 16:45:54 +0800

    update

--- src/Main.java
+++ src/Main.java
@@ -2,31 +2,67 @@ import java.util.Random;
 
 public class Main {
     // 测试使用q运行opCount个enqueue和dequeue操作所需时间，单位：秒
-    private static double testQueue(Queue<Integer> q, int opCount) {
-        long startTime = System.nanoTime();
-
+//    private static double testQueue(Queue<Integer> q, int opCount) {
+//        long startTime = System.nanoTime();
+//
+//        Random random = new Random();
+//        for (int i = 0; i < opCount; i++)
+//            q.enqueue(random.nextInt(Integer.MAX_VALUE));
+//        for(int i = 0; i < opCount; i++)
+//            q.dequeue();
+//
+//        long endTime = System.nanoTime();
+//
+//        return (endTime - startTime) / 1000000000.0;
+//    }
+//测试使用stack运行opCount个push和pop操作所需要的时间,单位:秒
+    private static double testStack(Stack<Integer> stack, int opCount) {
+        Long startTime = System.nanoTime();
         Random random = new Random();
-        for (int i = 0; i < opCount; i++)
-            q.enqueue(random.nextInt(Integer.MAX_VALUE));
-        for(int i = 0; i < opCount; i++)
-            q.dequeue();
-
+        for ( int i = 0; i < opCount; i++)
+            stack.push(random.nextInt(Integer.MAX_VALUE));
+        for(int i= 0;i< opCount;i ++)
+            stack.pop();
         long endTime = System.nanoTime();
-
         return (endTime - startTime) / 1000000000.0;
     }
 
     public static void main(String[] args) {
+        int opCount = 1000000;
 
-        int opCount = 100000;
+        ArrayStack<Integer> arrayStack = new ArrayStack<>();
+        double time1 = testStack(arrayStack, opCount);
+        System.out.println("ArrayStack, time: " + time1 + " s");
 
-        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
-        double time1 = testQueue(arrayQueue, opCount);
-        System.out.println("ArrayQueue, time:" + time1 + " s");
+        LinkedListStack<Integer> linkedListStack = new LinkedListStack<>();
+        double time2 = testStack(arrayStack, opCount);
+        System.out.println("LinkedListStack, time: " + time2 + " s");
 
-        LoopQueue<Integer> loopQueue = new LoopQueue<>();
-        double time2 = testQueue(loopQueue, opCount);
-        System.out.println("LoopQueue, time:" + time2 + " s");
+//        LinkedList<Integer> linkedList = new LinkedList<>();
+//        for (int i = 0; i < 5; i++) {
+//            linkedList.addFirst(i);
+//            System.out.println(linkedList);
+//        }
+//
+//        linkedList.add(2, 666);
+//        System.out.println(linkedList);
+//
+//        linkedList.remove(2);
+//        System.out.println(linkedList);
+//        linkedList.removeFirst();
+//        System.out.println(linkedList);
+//        linkedList.removeLast();
+//        System.out.println(linkedList);
+
+//        int opCount = 100000;
+//
+//        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
+//        double time1 = testQueue(arrayQueue, opCount);
+//        System.out.println("ArrayQueue, time:" + time1 + " s");
+//
+//        LoopQueue<Integer> loopQueue = new LoopQueue<>();
+//        double time2 = testQueue(loopQueue, opCount);
+//        System.out.println("LoopQueue, time:" + time2 + " s");
 
 //        ArrayStack<Integer> stack = new ArrayStack<>();
 //
