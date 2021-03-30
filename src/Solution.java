33489a55761d69a520921efa94ef95ffc3a48f84
Anjude <aboy007262@163.com>
2021-03-30 20:52:27 +0800

    2021/3/30

--- src/Solution.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-class Solution {
-    public List<Integer> addToArrayForm(int[] A, int K) {
-        List<Integer> list = new ArrayList<>();
-
-        return list;
-    }
-}
