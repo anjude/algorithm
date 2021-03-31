9a4e273f3c14817c90ddc8f39f1d5beb126cd8c0
Anjude <aboy007262@163.com>
2021-03-31 14:02:30 +0800

    q1025

--- src/Solution/DivisorGame.java
+++
@@ -1,7 +0,0 @@
-package Solution;
-
-/**
- * Created by Anjude on 2021/3/30 20:33
- */
-public class DivisorGame {
-}
---
+++ src/Solution/LeetCode/Q1025.java
@@ -0,0 +1,35 @@
+package Solution.LeetCode;
+
+/**
+ * Created by Anjude
+ * Date: 2021/3/31 13:16
+ */
+public class Q1025 {
+    public boolean divisorGame(int N) {
+        // 动态规划，有题目又说两个都是最佳状态我这理解就是每次x =1;
+        // 那么就有 如果n是爱丽丝赢 那么n-2是爱丽丝赢 状态转移方程 f(n) = !f(n-1)
+//        Boolean flag = true;
+//        for (int i = 1; i < N+1; i++) {
+//            flag = !flag;
+//        }
+//        return flag;
+
+        boolean[] f = new boolean[N + 5];
+
+        f[1] = false;
+        f[2] = true;
+
+        for (int i = 3; i <= N; ++i) {
+            for (int j = 1; j < i; ++j) {
+                if ((i % j) == 0 && !f[i - j]) {
+                    f[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return f[N];
+
+//        return n % 2 == 0;
+    }
+}
