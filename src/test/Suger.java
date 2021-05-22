8a43f8a33fcbc510949bb9337dec1a436001e63e
Anjude <aboy007262@163.com>
2021-05-22 23:14:52 +0800

    2021-5-22

---
+++ src/test/Suger.java
@@ -0,0 +1,16 @@
+package test;
+
+/**
+ * n天吃m块糖
+ * 后一天吃的糖是前一天的两倍
+ * 问第一天最多吃几块糖
+ *
+ * @author Anjude
+ * @date 2021/5/22 23:09
+ */
+public class Suger {
+    public int firstSugerNum(int n, int m) {
+        int firstDay = 0;
+        return firstDay;
+    }
+}
